07d432015fc93e4d1d5cb93bbcc13fe19072e6b7
root <jerry_hu123@163.com>
2017-08-28 21:28:11 +0800

    cmp0828

--- springcloudcmp/upgrade.sh
+++ springcloudcmp/upgrade.sh
@@ -316,146 +316,9 @@ EOF
 		echo_green "启动CMP完成..."
 }
 
-#安装单机版mysql5.7
-ssh-mysqlconnect(){
-    echo_green "建立对等互信开始..."
-        local ssh_init_path=./ssh-init.sh
-        $ssh_init_path $MYSQL_H
-        echo_green "建立对等互信完成..."
-        sleep 1
-}
-
-mysql_install(){
-	# echo_yellow "仅限于初始于安装！！"
-	echo_green "安装单机版mysql5.7.19开始"
-	local ostype=`check_ostype $MYSQL_H`
-	local os=`echo $ostype | awk -F _ '{print $1}'`
-        if [ "$os" == "centos" ]; then
-		local result=`ssh $MYSQL_H ps -ef | grep mysql | wc -l`
-		if [ "$result" -gt 1 ]; then
-			local mysql_v=`ssh $MYSQL_H mysql --version | sed -n '/5.7/p' | wc -l`
-			if [ "$mysql_v" -eq 1 ]; then
-				echo "mysql 5.7已安装"
-				exit
-			else
-				echo "请删除低版本备份好数据后，再执行最新版本的mysql安装"
-				exit
-			fi
-		fi
-		echo_yellow "安装依赖包"
-		local libaio=`ssh  "$MYSQL_H" rpm -qa |grep libaio |wc -l`
-		if [ "$libaio" -eq 1 ]; then
-			echo "libaio 已安装"
-		else
-			if [ "$ostype" == "centos_6" ]; then
-				 scp  ../packages/centos6_libaio/* "$MYSQL_H":/root/
-               			 ssh $MYSQL_H rpm -Uvh ~/libaio-0.3.107-10.el6.x86_64.rpm
-			elif [ "$ostype" == "centos_7" ]; then
-		        	 scp ../packages/centos7_libaio/* "$MYSQL_H":/root/
-                	 	 ssh $MYSQL_H rpm -Uvh ~/libaio-0.3.109-13.el7.x86_64.rpm
-			fi
-		fi
-		local numactl=`ssh "$MYSQL_H" rpm -qa |grep numactl |wc -l`
-		if [ "$numactl" -gt 0 ]; then
-                	echo "numactl 已安装"
-        	else
-                	if [ "$ostype" == "centos_6" ]; then
-               			scp ../packages/centos6_numactl/* "$MYSQL_H":/root/
-             		       	ssh $MYSQL_H rpm -Uvh ~/numactl-2.0.9-2.el6.x86_64.rpm
-               		 elif [ "$ostype" == "centos_7" ]; then
-				 scp ../packages/centos7_numactl/* "$MYSQL_H":/root/
-               			 ssh $MYSQL_H rpm -Uvh ~/numactl-2.0.9-6.el7_2.x86_64.rpm ~/numactl-libs-2.0.9-6.el7_2.x86_64.rpm
-               		 fi
-        	fi
-		local openssl=`ssh "$MYSQL_H" rpm -qa |grep openssl |wc -l`
-		if [ "$openssl" -gt 0 ]; then
-                	echo "openssl 已安装"
-        	else
-			if [ "$ostype" == "centos_6" ]; then
-         	        	scp ../packages/centos6_openssl/* "$MYSQL_H":/root/
-                		ssh $MYSQL_H rpm -Uvh ~/openssl-1.0.1e-57.el6.x86_64.rpm            
-			elif [ "$ostype" == "centos_7" ]; then
-    		        	scp ../packages/centos7_openssl/* "$MYSQL_H":/root/
-                		ssh $MYSQL_H rpm -Uvh ~/make-3.82-23.el7.x86_64.rpm  ~/openssl-1.0.1e-60.el7_3.1.x86_64.rpm  ~/openssl-libs-1.0.1e-60.el7_3.1.x86_64.rpm
-                	fi
-        	fi
-        	local iptables=`ssh  "$MYSQL_H" rpm -qa |grep iptables |wc -l`
-        	if [ "$iptables" -gt 0 ]; then
-                	echo "iptables 已安装"
-        	else
-                	if [ "$ostype" == "centos_6" ]; then
-                        	 scp  ../packages/centos6_iptables/* "$MYSQL_H":/root/
-                        	 ssh $MYSQL_H rpm -Uvh ~/iptables-1.4.7-16.el6.x86_64.rpm
-                	elif [ "$ostype" == "centos_7" ]; then
-                        	 scp ../packages/centos7_iptables/* "$MYSQL_H":/root/
-     				 ssh $MYSQL_H rpm -Uvh ~/iptables-1.4.21-17.el7.x86_64.rpm ~/libnetfilter_conntrack-1.0.6-1.el7_3.x86_64.rpm ~/libmnl-1.0.3-7.el7.x86_64.rpm ~/libnfnetlink-1.0.1-4.el7.x86_64.rpm
-                	fi
-        	fi
-	elif [ "$os" == "ubuntu" ]; then
-                        if [ "$ostype" == "ubuntu_12" ]; then
-                                echo_red "$ostype"暂不提供安装
-                                exit
-                        elif [ "$ostype" == "ubuntu_14" ]; then
-                                scp  ../packages/ubuntu14/* "$MYSQL_H":/root/
-           			ssh $MYSQL_H dpkg -i ~/libaio1_0.3.109-4_amd64.deb  ~/libnuma1_2.0.9~rc5-1ubuntu3.14.04.2_amd64.deb  ~/openssl_1.0.1f-1ubuntu2.22_amd64.deb ~/iptables_1.4.21-1ubuntu1_amd64.deb ~/libnfnetlink0_1.0.1-2_amd64.deb ~/libxtables10_1.4.21-1ubuntu1_amd64.deb
-                        elif [ "$ostype" == "ubuntu_16" ]; then
-                                echo_red "$ostype"暂不提供安装
-                                exit
-                        else
-                                echo_red "$ostype"暂不提供安装
-                                exit
-                        fi
-        fi
-		echo_green "复制文件"
-	scp -r ../packages/mysql-5.7.19 "$MYSQL_H":/usr/local
-	ssh $MYSQL_H <<EOF
-		echo "创建mysql用户"
-		mv /usr/local/mysql-5.7.19 /usr/local/mysql
-		groupadd mysql
-		useradd -r -g mysql -s /bin/false mysql
-		echo "修改文件权限"
-		chown -R mysql.mysql /usr/local/mysql
-		chmod 744 /usr/local/mysql/bin/*
-		cp /usr/local/mysql/support-files/my-default.cnf /etc/my.cnf
-		cp /usr/local/mysql/support-files/mysql.server /etc/init.d/mysql
-		chmod 744 /etc/init.d/mysql
-		echo "初始化MYSQL"
-		cd /usr/local/mysql/bin
-		./mysqld --initialize-insecure --user=mysql --basedir=/usr/local/mysql --datadir=/usr/local/mysql/data
-		echo "给数据库加密"
-		./mysql_ssl_rsa_setup --user=mysql --datadir=/usr/local/mysql/data
-		chmod 644 /usr/local/mysql/data/server-key.pem
-		echo "第一次启动MYSQL"
-		/etc/init.d/mysql restart
-		#./mysqld_safe --user=mysql &
-		echo "配置开机启动"
-		chkconfig --add mysql
-		echo "配置环境变量"
-		sed -i /mysql/d /etc/profile
-		echo export PATH=/usr/local/mysql/bin:'\$PATH' >>/etc/profile
-		source /etc/profile
-		exit
-EOF
-		scp ./init_mysql.sh "$MYSQL_H":/root/
-		#分别为ROOT密码，EVUSER密码，IM密码。
-		MYSQL_PASS=("$MYSQL_ROOT_PASSWORD" "$MYSQL_EVUSER_PASSWORD" "$MYSQL_IM_PASSWORD")
-		ssh $MYSQL_H /root/init_mysql.sh "${MYSQL_PASS[@]}"
-		 
-		
-	
-	echo_green "安装完成"
-}
-#mysql服务器iptables配置
-iptables-mysql(){
-  	echo_green "配置iptables开始..."
-        local iptable_path=./iptablesmysql.sh
-        $iptable_path $MYSQL_H
-	echo_green "配置iptables完成..."
-}
 
 echo_yellow "-----------一键安装（增量）说明-------------------"
 echo_yellow "1、可安装JDK1.8.0_131软件;"
-#echo_yellow "2、可安装MYSQL5.7.19软件;"
 echo_yellow "2、可安装有iptables lsof软件;"
 echo_yellow "3、初始化时，建议使用root用户安装;"
 echo_yellow "4、确保.sh有执行权限，并且使用 ./xxx.sh执行;"
@@ -468,7 +331,6 @@ echo "1-----allinone服务器,每台32G内存."
 echo "2-----3台服务器,每台16G内存.2台控制节点，1台采集节点"  
 echo "3-----4台服务器,每台16G内存.3台控制节点，1台采集节点"  
 echo "4-----6台服务器,每台8G内存.5台控制节点，1台采集节点"
-#echo "5-----安装单机版mysql5.7"
 
 while read item
 do
